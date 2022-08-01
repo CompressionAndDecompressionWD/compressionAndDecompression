57650faee13ba78454cabe86e4467e820e9d6966
CohenAyala <ayala40565@gmail.com>
2022-08-01 22:05:02 +0300

    define functions

--- projectWD/compress_and_decompress.c
+++ projectWD/compress_and_decompress.c
@@ -1,1 +1,48 @@
 #include "compress_and_decompress.h"
+#define nullptr NULL
+FILE* compress(FILE* code_file)
+{
+    return nullptr;
+}
+
+int* freq_count(FILE* code_file)
+{
+    return nullptr;
+}
+
+void build_huffman_tree(int* freq_arr, char* char_arr)
+{
+}
+
+Min_heap_node* create_new_node(int freq, char c)
+{
+    return nullptr;
+}
+
+int is_one_leaf(Min_heap_node* heap)
+{
+    return 0;
+}
+
+void insert_min_heap(Min_heap_node** heap, Min_heap_node* node)
+{
+}
+
+Min_heap_node* extractmin(Min_heap_node** heap)
+{
+    return nullptr;
+}
+
+char** huffman_code(Min_heap_node** root)
+{
+    return nullptr;
+}
+
+Min_heap_node** build_min_heap(int* freq_arr, char* char_arr)
+{
+    return nullptr;
+}
+
+void swap(Min_heap_node* a, Min_heap_node* b)
+{
+}
--- projectWD/compress_and_decompress.h
+++ projectWD/compress_and_decompress.h
@@ -1,5 +1,25 @@
 
 #ifndef compress_and_decompress
 #define compress_and_decompress
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct min_heap_node {
+	char c;
+	int freq;
+	Min_heap_node* left;
+	Min_heap_node* right;
+}Min_heap_node;
+//compression
+FILE* compress(FILE*);
+int* freq_count(FILE*);
+Min_heap_node** build_huffman_tree(int*, char*);
+Min_heap_node* create_new_node(int, char);
+int is_one_leaf(Min_heap_node*);
+void min_heapify(Min_heap_node**);
+void insert_min_heap(Min_heap_node**, Min_heap_node*);
+Min_heap_node* extractmin(Min_heap_node**);
+char** huffman_code(Min_heap_node**);
+Min_heap_node** build_min_heap(int*, char*);
+void swap(Min_heap_node*, Min_heap_node*);
 
 #endif
