e86767121946976bd623fc97fc10d3b4458b49ed
CohenAyala <ayala40565@gmail.com>
2022-08-02 15:40:08 +0300

    function compress

--- projectWD/compress_and_decompress.c
+++ projectWD/compress_and_decompress.c
@@ -1,27 +1,79 @@
 #include "compress_and_decompress.h"
+#include <string.h>
+#include <math.h>
 #define nullptr NULL
-FILE* compress(FILE* code_file)
+#define _CRT_SECURE_NO_WARNINGS
+FILE* compress(char* code_file)
 {
-    return nullptr;
+	FILE* sourse_file = fopen(code_file, "r");
+	if (sourse_file == NULL) {
+		//to do ...
+		exit(1);
+	}
+	int* freq_arr = freq_count(code_file);
+	Min_heap_node** huffman_tree = build_huffman_tree(freq_arr);
+	char** huffman_array = huffman_code(huffman_tree);
+	char* file_name=NULL;
+	_strdup(file_name, code_file);
+	strcat(file_name, ".bin");
+	FILE* compressed_file = fopen(file_name, "wb");
+	if (compressed_file == NULL)
+		exit(1);
+	//todo...
+	replace(sourse_file, compressed_file, huffman_array);
+}
+
+FILE* replace(FILE* sourse_file,FILE* compressed_file,int * huffman_array)
+{
+
+	int ch, temp, i = 0, index_code = 0;
+	char char_code;
+	char* code;
+	ch = fgetc(sourse_file);
+	do
+	{
+		code = huffman_array[ch];
+		while (index_code < strlen(code) && i < 8)
+		{
+			temp += code[index_code] * pow(2, index_code % 8);
+			i++;
+			index_code++;
+		}
+
+		if (index_code <= strlen(code)) {
+			//finish temp and not finish code
+			char_code = temp;
+			fwrite(&char_code, 1, 1, compressed_file);
+			i = 0;
+			temp = 0;
+		}
+
+		if (i <= 8) {
+			//finish code
+			ch = fgetc(sourse_file);
+			index_code = 0;
+		}
+	} while (ch != EOF);
+
 }
 
 int* freq_count(FILE* code_file)
 {
-    return nullptr;
+	return nullptr;
 }
 
-void build_huffman_tree(int* freq_arr, char* char_arr)
+Min_heap_node** build_huffman_tree(int* freq_arr)
 {
 }
 
 Min_heap_node* create_new_node(int freq, char c)
 {
-    return nullptr;
+	return nullptr;
 }
 
 int is_one_leaf(Min_heap_node* heap)
 {
-    return 0;
+	return 0;
 }
 
 void insert_min_heap(Min_heap_node** heap, Min_heap_node* node)
@@ -30,17 +82,17 @@ void insert_min_heap(Min_heap_node** heap, Min_heap_node* node)
 
 Min_heap_node* extractmin(Min_heap_node** heap)
 {
-    return nullptr;
+	return nullptr;
 }
 
 char** huffman_code(Min_heap_node** root)
 {
-    return nullptr;
+	return nullptr;
 }
 
 Min_heap_node** build_min_heap(int* freq_arr, char* char_arr)
 {
-    return nullptr;
+	return nullptr;
 }
 
 void swap(Min_heap_node* a, Min_heap_node* b)
--- projectWD/compress_and_decompress.h
+++ projectWD/compress_and_decompress.h
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 
 #ifndef compress_and_decompress
 #define compress_and_decompress
@@ -6,13 +7,14 @@
 typedef struct min_heap_node {
 	char c;
 	int freq;
-	Min_heap_node* left;
-	Min_heap_node* right;
+	struct min_heap_node* left;
+	struct min_heap_node* right;
 }Min_heap_node;
 //compression
-FILE* compress(FILE*);
+FILE* compress(char*);
+FILE* replace(FILE*,FILE*,int*);
 int* freq_count(FILE*);
-Min_heap_node** build_huffman_tree(int*, char*);
+Min_heap_node** build_huffman_tree(int*);
 Min_heap_node* create_new_node(int, char);
 int is_one_leaf(Min_heap_node*);
 void min_heapify(Min_heap_node**);
