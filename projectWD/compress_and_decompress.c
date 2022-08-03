9de0d3838d21bff5a5967ce71edd841e4f5d8c8a
rivka-fruchter <r0556779798@gmail.com>
2022-08-03 16:29:40 +0300

    rename

--- projectWD/compress_and_decompress.c
+++ projectWD/compress_and_decompress.c
@@ -5,22 +5,26 @@
 #define nullptr NULL
 #define _CRT_SECURE_NO_WARNINGS
 #define LOG_FILE "history.log"
+
+#define CHAR_TO_TRUCK 'a'
+//איך כותבים את השמירת היסטוריה הזאת??
 void stack_int_init(S_int* S)
 {
 	S->arr = (int*)malloc(sizeof(int));
 	S->size = 0;
 }
-void stack_push_int(S_int* S, int num)
+void stack_int_push(S_int*S, int num)
 {
+	
 	S->size ++;
 	S->arr = (int*)realloc(S->arr,sizeof(int)*S->size);
 	S->arr[S->size - 1] = num;
 	
 }
-int pop_int(S_int* S) {
+int stack_int_pop(S_int* S) {
 
 }
-int isEmpty_int(S_int* S) {
+int stack_int_isEmpty(S_int* S) {
 
 }
 void stack_node_push(SNode** top_ref, Min_heap_node* t)
@@ -83,6 +87,11 @@ FILE* compress_main(char* code_file)
 	Min_heap* huffman_tree = build_huffman_tree(freq_arr);
 
 	char** huffman_array = huffman_code(huffman_tree);
+
+	int* freq_arr = compress_build_freq_array(sourse_file);
+	Min_heap* huffman_tree = compress_build_huffman_tree(freq_arr);
+	int*huffman_dictionary_char_codes = compress_build_huffman_codes_dictionary(huffman_tree);
+
 	char* file_name = NULL;
 	_strdup(file_name, code_file);
 	strcat(file_name, ".bin");
@@ -90,11 +99,11 @@ FILE* compress_main(char* code_file)
 	if (compressed_file == NULL)
 		exit(1);
 	//todo...
-	replace(sourse_file, compressed_file, huffman_array);
+	compress_replace_chars_to_huffman_codes_in_file(sourse_file, compressed_file, huffman_array);
 	return compressed_file;
 }
 
-FILE* replace(FILE* sourse_file, FILE* compressed_file, char** huffman_array)
+FILE* compress_replace_chars_to_huffman_codes_in_file(FILE* sourse_file, FILE* compressed_file, int* huffman_array)
 {
 
 	int ch, temp, i = 0, index_code = 0;
@@ -128,7 +137,7 @@ FILE* replace(FILE* sourse_file, FILE* compressed_file, char** huffman_array)
 
 }
 
-int* freq_count(FILE* code_file)
+int* compress_build_freq_array(FILE* code_file)
 {
 	int* arr_freq = NULL;
 	arr_freq = (int*)calloc(255, sizeof(int));
@@ -141,22 +150,22 @@ int* freq_count(FILE* code_file)
 	return arr_freq;
 }
 
-Min_heap* build_huffman_tree(int* freq_arr)
+Min_heap* compress_build_huffman_tree(int* freq_arr)
 {
-	Min_heap* min_heap = build_min_heap(freq_arr);
-	while (is_one_leaf(min_heap))
+	Min_heap* min_heap = compress_build_min_heap(freq_arr);
+	while (compress_heap_is_one_leaf(min_heap))
 	{
-		Min_heap_node* right = extractmin(min_heap);
-		Min_heap_node* left = extractmin(min_heap);
-		Min_heap_node* new_internal_node = create_new_node(right->freq + left->freq, '$');
+		Min_heap_node* right = compress_min_heap_extractmin(min_heap);
+		Min_heap_node* left = compress_min_heap_extractmin(min_heap);
+		Min_heap_node* new_internal_node = compress_create_new_min_heap_node(right->freq + left->freq, '$');
 		new_internal_node->right = right;
 		new_internal_node->left = left;
-		insert_min_heap(min_heap, new_internal_node);
+		compress_min_heap_insert(min_heap, new_internal_node);
 	}
 	return min_heap;
 }
 
-Min_heap_node* create_new_node(int freq, char c)
+Min_heap_node* compress_create_new_min_heap_node(int freq, char c)
 {
 	Min_heap_node* node = NULL;
 	node = (Min_heap_node*)malloc(sizeof(Min_heap_node));
@@ -165,11 +174,11 @@ Min_heap_node* create_new_node(int freq, char c)
 	return node;
 }
 
-int is_one_leaf(Min_heap* heap)
+int compress_heap_is_one_leaf(Min_heap* heap)
 {
 	return heap->size > 1;
 }
-void min_heapify(Min_heap* h, int index)
+void compress_min_heapify(Min_heap* h, int index)
 {
 	int left_index = index * 2 + 1;
 	int right_index = index * 2 + 2;
@@ -180,12 +189,12 @@ void min_heapify(Min_heap* h, int index)
 		smallest = right_index;
 	if (smallest != index)
 	{
-		swap(&h->arr[index], &h->arr[smallest]);
-		min_heapify(h, smallest);
+		compress_heap_node_swap(&h->arr[index], &h->arr[smallest]);
+		compress_min_heapify(h, smallest);
 	}
 }
 
-void insert_min_heap(Min_heap* heap, Min_heap_node* node)
+void compress_min_heap_insert(Min_heap* heap, Min_heap_node* node)
 {
 	heap->size++;
 	heap->arr = (Min_heap_node**)realloc(heap->arr, sizeof(Min_heap_node*)*heap->size);
@@ -194,31 +203,60 @@ void insert_min_heap(Min_heap* heap, Min_heap_node* node)
 		//to do
 	}
 	heap->arr[heap->size - 1] = node;
-	min_heapify(heap, 0);
+	compress_min_heapify(heap, 0);
 }
 
-Min_heap_node* extractmin(Min_heap* heap)
+Min_heap_node* compress_min_heap_extractmin(Min_heap* heap)
 {
 	Min_heap_node* node = heap->arr[0];
 	heap->arr[0] = heap->arr[heap->size-1];
 	free(heap->arr[heap->size-1]);
-	min_heapify(heap, 0);
+	compress_min_heapify(heap, 0);
 	heap->size--;
 	return node;
 }
 
-char** huffman_code(Min_heap* root)
+int* compress_build_huffman_codes_dictionary(Min_heap* root)
 {
 	char** huffman_codes = NULL;
 	huffman_codes = (char**)malloc(sizeof(char*));
 	//if the tree has 1 node
 	huffman_codes[0] = "0";
-
 	 Min_heap_node* current = root->arr[0];
 	 SNode* node_stack = NULL; 
 	 S_int* code_stack = NULL; 
 	 stack_int_init(code_stack);
 	int done = 0;
+	Min_heap_node* current = root->arr[0];
+	SNode* node_stack = NULL;
+	S_int* code_stack = NULL;
+	stack_int_init(code_stack);
+	int done = 0;
+	while (!done)
+	{
+		if (!current->left && !current->right) {
+			// output the code stack to huffman array
+		}
+		if (current != NULL)
+		{
+			stack_node_push(&node_stack, current);
+			stack_push_int(code_stack, 0);
+			current = current->left;
+		}
+		else
+		{
+			if (!stack_node_is_empty(node_stack))
+			{
+				current = pop(&node_stack);
+				stack_int_pop(code_stack);
+				current = current->right;
+			}
+			else
+			{
+				done = 1;
+			}
+		}
+	}
 
 	while (!done)
 	{
@@ -236,7 +274,7 @@ char** huffman_code(Min_heap* root)
 			if (!stack_node_is_empty(node_stack))
 			{
 				current = pop(&node_stack);
-				pop_int(code_stack);
+				stack_int_pop(code_stack);
 				current = current->right;
 			}
 			else
@@ -247,54 +285,56 @@ char** huffman_code(Min_heap* root)
 	} 
 }
 
+Min_heap* compress_build_min_heap(int* freq_arr)
 
-Min_heap* build_min_heap(int* freq_arr)
 {
 	Min_heap* min_heap;
-	init_min_heap(min_heap);
+	compress_init_min_heap(min_heap);
+	//create node for each charcter+freq
 	for (int i = 0; i < sizeof(freq_arr)/sizeof(int); i++)
 	{
 		if (freq_arr[i] != 0) {
 			min_heap->arr = (Min_heap_node**)realloc(min_heap->arr, sizeof(Min_heap_node*) * min_heap->size);
-			min_heap->arr[min_heap->size++]= create_new_node(freq_arr[i], (char)i);
+			min_heap->arr[min_heap->size++]= compress_create_new_min_heap_node(freq_arr[i], (char)i);
 		}
 	}
+	//build the heap
 	for (int i = min_heap->size; i > min_heap->size/2; i++)
 	{
-		min_heapify(min_heap, i);
+		compress_min_heapify(min_heap, i);
 	}
 	return min_heap;
 }
 
-void init_min_heap(Min_heap* min_heap)
+void compress_init_min_heap(Min_heap* min_heap)
 {
 	min_heap->size = 0;
 	min_heap->capacity = 255;
     min_heap->arr = (Min_heap_node**)malloc(sizeof(Min_heap_node*));
 }
 
-void swap(Min_heap_node* a, Min_heap_node* b)
+void compress_heap_node_swap(Min_heap_node* a, Min_heap_node* b)
 {
 	Min_heap_node temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void keep_history(char* function, char* data)
-{
-	time_t t;   // not a primitive datatype
-	time(&t);
-	char* date_time = ctime(&t);
-	FILE* log_file = fopen(LOG_FILE, "w");
-	char* history = NULL;
-	history = _strdup(date_time);
-	history = (char*)realloc(history, sizeof(history) + sizeof(function) + sizeof(data) + 23);
-	strcat(history, " : ");
-	strcat(history, " function: ");
-	strcat(history, function);;
-	strcat(history, " state: ");
-	strcat(history, data);
-	strcat(history, "\0");
-	fputs( history, log_file);
-	fclose(log_file);
-}
+//void keep_history(char* function, char* data)
+//{
+//	time_t t;   // not a primitive datatype
+//	time(&t);
+//	char* date_time = ctime(&t);
+//	FILE* log_file = fopen(LOG_FILE, "w");
+//	char* history = NULL;
+//	history = _strdup(date_time);
+//	history = (char*)realloc(history, sizeof(history) + sizeof(function) + sizeof(data) + 23);
+//	strcat(history, " : ");
+//	strcat(history, " function: ");
+//	strcat(history, function);;
+//	strcat(history, " state: ");
+//	strcat(history, data);
+//	strcat(history, "\0");
+//	fputs( history, log_file);
+//	fclose(log_file);
+//}
